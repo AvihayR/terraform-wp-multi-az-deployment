a80c623c572f094df302869d7263f538d55f0fc7
AvihayR <avihayrabi@gmail.com>
2025-05-16 15:24:34 +0300

    Add wordpress installation shell script

---
+++ scripts/install_wp.sh
@@ -0,0 +1,9 @@
+# Install LAMP Stack
+sudo yum update -y
+sudo amazon-linux-extras install mariadb10.5
+sudo amazon-linux-extras install php8.2
+sudo yum install -y httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
+
+
