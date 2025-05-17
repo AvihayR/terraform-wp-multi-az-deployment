6ae1e7a01b5f3e4d4e0b4b3b4ee56be985e714b2
AvihayR <avihayrabi@gmail.com>
2025-05-18 00:07:31 +0300

    Wordpress up and running

--- scripts/install_wp.sh
+++ scripts/install_wp.sh
@@ -1,36 +1,34 @@
-#!/bin/bash
-# Install LAMP Stack
-sudo yum update -y
-sudo amazon-linux-extras install mariadb10.5
-sudo amazon-linux-extras install php8.2
-sudo yum install -y httpd
-sudo systemctl start httpd
-sudo systemctl enable httpd
+    #!/bin/bash
+    sudo yum update -y
+    sudo amazon-linux-extras install -y mariadb10.5
+    sudo amazon-linux-extras install -y php8.2
+    sudo yum install -y httpd
+    sudo systemctl start httpd
+    sudo systemctl enable httpd
+
+    sudo usermod -a -G apache ec2-user
+    sudo chown -R ec2-user:apache /var/www
+    sudo chmod 2775 /var/www && find /var/www -type d -exec sudo chmod 2775 {} \;
+    find /var/www -type f -exec sudo chmod 0664 {} \;
+
+    wget https://wordpress.org/latest.tar.gz
+    tar -xzf latest.tar.gz
+    # sudo mv wordpress/* /var/www/html/
 
 
-# Change permissions and own apache's directory
-sudo usermod -a -G apache ec2-user
-sudo chown -R ec2-user:apache /var/www
-sudo chmod 2775 /var/www && find /var/www -type d -exec sudo chmod 2775 {} \;
-find /var/www -type f -exec sudo chmod 0664 {} \;
+    # Write SQL commands to a temp file
+    DB_USER="${var.db_username}"
+    DB_PASS="${var.db_password}"
+    DB_HOST="${module.rds.endpoint}"
 
-
-# Test if php works in apache's directory 
-# echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
-
-
-# Download wp
-wget https://wordpress.org/latest.tar.gz
-tar -xzf latest.tar.gz
-touch /home/ec2-user/hello.txt
-
-# Create a db for wp
-mysql -u${var.db_username} -p${var.db_password} -h ${module.rds.endpoint} -e <<-EOF
-    CREATE DATABASE `wordpress-db`;
-    GRANT ALL PRIVILEGES ON `wordpress-db`.* TO "${var.db_username}"@"${module.rds.endpoint}";
+    cat <<SQL > /tmp/init_wp.sql
+    CREATE DATABASE \`wordpress-db\`;
+    GRANT ALL PRIVILEGES ON \`wordpress-db\`.* TO '$DB_USER'@'%';
     FLUSH PRIVILEGES;
-EOF
+    SQL
+
+    # Run the SQL script
+    mysql -u"$DB_USER" -p"$DB_PASS" -h "$DB_HOST" < /tmp/init_wp.sql
 
 
-# Edit wp-config file
-cp wordpress/wp-config-sample.php wordpress/wp-config.php
+    
