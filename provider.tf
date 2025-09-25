3ca194f130ca88478ea949b7ed41273647f0b3c0
AvihayR <avihayrabi@gmail.com>
2025-09-25 23:11:12 +0300

    Add remote state S3 bucket configuration

--- provider.tf
+++ provider.tf
@@ -1,4 +1,13 @@
 terraform {
+    backend "s3" {
+    bucket         = "arize-tf-state-management"
+    key            = "terraform.tfstate"
+    region         = "eu-central-1"
+    use_lockfile   = true
+    encrypt        = true
+  }
+
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
