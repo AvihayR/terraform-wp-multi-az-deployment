1314d2d6ccd55d660fc01742cd71d6952738b12f
AvihayR <avihayrabi@gmail.com>
2025-10-07 20:32:08 +0300

    Configure logs for ECS Task Defs

--- modules/ecs/ecs.tf
+++ modules/ecs/ecs.tf
@@ -39,16 +39,33 @@ resource "aws_ecs_task_definition" "wp-task" {
           "name"  = "WORDPRESS_TABLE_PREFIX"
           "value" = "wp"
         },
-      ]
+      ],
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = aws_cloudwatch_log_group.ecs_logs.name
+          awslogs-region        = "eu-central-1"
+          awslogs-stream-prefix = "wp-ecs"
+        }
+      }
     }
   ])
 }
 
 resource "aws_ecs_service" "wp-service" {
-  name            = "Wordpress-service"
+  name            = "wp-service"
   cluster         = aws_ecs_cluster.wp-cluster.id
   task_definition = aws_ecs_task_definition.wp-task.arn
   desired_count   = 2
+
+  deployment_controller {
+    type = "CODE_DEPLOY"
+  }
+}
+
+resource "aws_cloudwatch_log_group" "ecs_logs" {
+  name              = aws_ecs_service.wp-service.name
+  retention_in_days = 2
 }
 
 output "name" {
